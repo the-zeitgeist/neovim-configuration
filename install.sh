6877781596b645fa8065441173829d743aa8e3f5
Zeitgeist <shecksk8nick@gmail.com>
2022-08-13 09:52:40 -0500

    Completed setup script

--- install.sh
+++ install.sh
@@ -1,2 +1,36 @@
-# nvim -N -u NONE -n -c "set nomore" -S "install.vim" "filespect"
-vim -T dumb --noplugin -n -es -S "install.vim" "filespec"
+#!/bin/bash
+
+# This script is meant to be used when configuring vim / neovim for the first time
+# If you already have a .vimrc file working, you need to copy this file manually
+
+bin=$(command -v nvim || command -v vim)
+echo $bin
+
+if [ -z "$bin" ]; then
+	echo 'vim is not installed'
+	exit 1
+fi
+
+conf=$(ls -a ~/. | grep .vimrc)
+
+if [ -z "$conf" ]; then
+	echo 'creating symbolic link to vimrc'
+	mkdir -p ~/.vim ~/.vim/autoload ~/.vim/plugged ~/.config/nvim
+	echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after \let &packpath = &runtimepath \source ~/.vimrc' > ~/.config/nvim/init.vim
+	ln -s .vimrc ~/
+fi
+
+echo 'installing vim plug'
+if [[ "$bin" == *"nvim" ]]; then
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+else
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+vim +PlugInstall
+vim +CocInstall coc-prettier coc-pairs coc-jest coc-git coc-eslint coc-yaml coc-tslint
+vim +CocInstall coc-json coc-go coc-java coc-sh coc-python
+
+echo 'Done :)'
--- install.vim
+++
@@ -1,9 +0,0 @@
-echo 'Installing extensions'
-
-:!sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-:PlugInstall
-echo "Installing completition features"
-:CocInstall coc-tabnine coc-pairs coc-lists coc-html coc-git coc-eslint coc-yaml coc-tslint coc-sh coc-rome coc-python coc-json coc-go coc-css
-" :q
